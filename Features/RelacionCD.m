b28a403ef832488354b5b08fd61976f6634398e8
Victoria Guarnieri <vickyguarnieri1@gmail.com>
2023-10-25 18:12:12 -0300

    Add files via upload
    RelacionCD: relación entre el área del disco óptico y la copa óptico
    RelacionDH: relación entre la distancia del centro del disco óptico y la cabeza del nervio óptico con el diámetro del disco óptico
    RelaciónVG: relación del área total de los vasos sanguíneos con el globo ocular

---
+++ Features/RelacionCD.m
@@ -0,0 +1,66 @@
+function [CD,cont] = RelacionCD(im,cont)
+im = imresize(im,[560 720]); 
+
+% Componentes roja y verde
+R = im(:,:,1); 
+G = im(:,:,2); 
+
+% Ajuste de intensidad
+G_i = imadjust(G); 
+R_i = imadjust(R);
+
+% Elemento estructurante tamaño 10
+EE = strel('disk',10);  
+
+% Operaciones morfologicas -------
+
+% Cerradura
+R = imclose(R_i,EE);        
+G = imclose(G_i,EE);           
+
+% Aperura
+R = imopen(R,EE);   
+G = imopen(G,EE);
+R = imadjust(R); 
+
+% Umbralizacion ------------------
+R_bw = im2bw(R, 0.98); 
+G_bw = im2bw(G, 0.999);
+
+%Elemento estructurante tamaño 20
+EE = strel('disk',20);  
+
+% Erosion
+R_bw = imerode(R_bw,EE);
+G_bw = imerode(G_bw,EE);
+
+% Dilatacion
+R_bw = imdilate(R_bw,EE);
+
+%Elemento estructural tamaño 10
+EE = strel('disk', 10); 
+G_bw = imdilate(G_bw,EE);
+
+A_disco = sum(R_bw(:));
+A_copa = sum(G_bw(:));
+CD = imdivide(A_copa, A_disco);
+
+if(sum(G_bw) == 0) % se nos hizo cero el cup
+    CD = AjusteCup(G_i,G_bw,R_bw);
+end
+% TODOOOOOO!!!!!!!
+% else %el disk es muy grande
+%         %AjusteDisk(R_i,R_bw);
+% end
+
+
+% Componente roja
+% figure(1)
+% subplot(231),imshow(im),title(['Imagen normal',num2str(CD)])
+% subplot(232),imshow(R),title('Componente roja')
+% subplot(233),imshow(R_bw),title('Area del disco')
+% subplot(234),imshow(G),title('Componente verde')
+% subplot(235),imshow(G_bw),title('Cup Area')
+% pause(0.2)
+
+end
