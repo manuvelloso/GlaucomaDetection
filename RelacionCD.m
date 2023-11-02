843158f9877537d94bfcf34719c2bc8f4b8bc461
Manuela Velloso <65735915+manuvelloso@users.noreply.github.com>
2023-11-02 17:30:52 -0300

    Update RelacionCD.m

--- RelacionCD.m
+++ RelacionCD.m
@@ -1,64 +1,86 @@
 function [CD] = RelacionCD(im)
-im = imresize(im,[560 720]); 
 
 % Componentes roja y verde
-R = im(:,:,1); 
-G = im(:,:,2); 
+% R = im(:,:,1); 
+% G = im(:,:,2); 
 
 % Ajuste de intensidad
-G_i = imadjust(G); 
-R_i = imadjust(R);
+% G_i = imadjust(G); 
+% R_i = imadjust(R);
 
-% Elemento estructurante tamaño 15
-EE = strel('disk',10);  
+% Elemento estructurante tamaÃ±o 15
+% EE = strel('disk',10);  
 
 % Operaciones morfologicas -------
-
 % Cerradura
-R = imclose(R_i,EE);        
-G = imclose(G_i,EE);           
+% R = imclose(R_i,EE);        
+% G = imclose(G_i,EE);           
 
 % Aperura
-R = imopen(R,EE);   
-G = imopen(G,EE);
-R = imadjust(R); 
+% R = imopen(R,EE);   
+% G = imopen(G,EE);
+% R = imadjust(R); 
 
 % Umbralizacion ------------------
-R_bw = im2bw(R, 0.98); 
-G_bw = im2bw(G, 0.999);
+% R_bw = im2bw(R, 0.98); 
+% G_bw = im2bw(G, 0.999);
 
-%Elemento estructurante tamaño 20
-EE = strel('disk',20);  
+% Elemento estructurante tamaÃ±o 20
+% EE = strel('disk',20);  
 
 % Erosion
-R_bw = imerode(R_bw,EE);
-G_bw = imerode(G_bw,EE);
+% R_bw = imerode(R_bw,EE);
+% G_bw = imerode(G_bw,EE);
 
 % Dilatacion
-R_bw = imdilate(R_bw,EE);
+% R_bw = imdilate(R_bw,EE);
 
-%Elemento estructural tamaño 10
-EE = strel('disk',10); 
-G_bw = imdilate(G_bw,EE);
+%Elemento estructural tamaÃ±o 10
+% EE = strel('disk',10); 
+% G_bw = imdilate(G_bw,EE);
 
-A_disco = sum(R_bw(:));
-A_copa = sum(G_bw(:));
-CD = imdivide(A_copa,A_disco);
+% A_disco = sum(R_bw(:));
+% A_copa = sum(G_bw(:));
+% CD = imdivide(A_copa,A_disco);
 
+% figure(1)
+% subplot(231),imshow(im),title(['Imagen normal',num2str(CD)])
+% subplot(232),imshow(R),title('Componente roja')
+% subplot(233),imshow(R_bw),title('Area del disco')
+% subplot(234),imshow(G),title('Componente verde')
+% subplot(235),imshow(G_bw),title('Cup Area')
+% pause(0.1)
+
+% if(sum(G_bw)==0) % se nos hizo cero el cup
+   %[CD,G_bw]=AjusteCup(G_i,G_bw,R_bw);
+% end
+% if(CD<0.06) %disk es muy grande
+    %CD=funcion(R_i,G_bw);
+%end
+
+G = im(:,:,2); 
+G_i = imadjust(G);
+
+Disk_bw = MascaraDisco(im);
+Cup_bw = MascaraCup(im);
+
+if(sum(Cup_bw)==0) % se nos hizo cero el cup
+   [CD,Cup_bw]=AjusteCup(G_i,Cup_bw,Disk_bw);
+end
+
+% Componente roja
 figure(1)
-subplot(231),imshow(im),title(['Imagen normal',num2str(CD)])
-subplot(232),imshow(R),title('Componente roja')
-subplot(233),imshow(R_bw),title('Area del disco')
-subplot(234),imshow(G),title('Componente verde')
-subplot(235),imshow(G_bw),title('Cup Area')
-pause(0.1)
+subplot(121)
+imshowpair(Disk_bw,Cup_bw);
+subplot(122)
+imshow(im);
+pause(0.3);
 
-if(sum(G_bw)==0) % se nos hizo cero el cup
-   [CD,G_bw]=AjusteCup(G_i,G_bw,R_bw);
-end
-if(CD<0.06) %disk es muy grande
-    CD=funcion(R_i,G_bw);
-end
+A_disco = sum(Disk_bw(:));
+A_copa = sum(Cup_bw(:));
 
+
+%% Relacion copa al disco    
+CD = imdivide(A_copa,A_disco);
 
 end
