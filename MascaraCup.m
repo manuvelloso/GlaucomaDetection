38397d14c8914add09104c72b5715814df76bd3e
Manuela Velloso <65735915+manuvelloso@users.noreply.github.com>
2023-11-02 18:28:45 -0300

    Add files via upload

---
+++ MascaraCup.m
@@ -0,0 +1,19 @@
+function C = MascaraCup(im)
+C = im(:,:,2); 
+C = imadjust(C); 
+
+% Elemento estructurante tamaño 10
+EE = strel('disk',5); 
+
+C = imclose(C,EE);
+C = imopen(C,EE);
+
+%Elemento estructurante tamaño 20
+EE = strel('disk',7); 
+
+C = im2bw(C, 0.999);
+C = imerode(C,EE);
+
+EE = strel('disk',5); 
+C = imdilate(C,EE);
+end
