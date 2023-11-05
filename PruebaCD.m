2dea367bc1e826da4102289ad7144ead0a114d11
MailenZino <84191140+MailenZino@users.noreply.github.com>
2023-11-05 15:51:01 -0300

    Add files via upload

---
+++ PruebaCD.m
@@ -0,0 +1,39 @@
+function PruebaCD(im,A) 
+B=im(:,:,3);
+
+%obtencion del disco
+D = MascaraDisco(im); 
+
+%obtencion del cup
+[C,~,~]=NormalizarBlue(im,A);
+
+[center, radio] = imfindcircles(D,[15 70]);
+if (isempty(center)||isempty(radio))
+    D = OtraMascaraDisco(im);
+    stats = regionprops(D,'Centroid','MajorAxisLength','MinorAxisLength');
+    center=stats.Centroid;
+    diametro = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
+    radio = diametro/2;
+end
+
+if (sum(sum(C))<50)
+    [C,~,~]=NormalizarBlue(im,0.18);
+end
+
+M = zeros(size(C));                         %armo matriz de ceros
+M(round(center(1,2)),round(center(1,1))) = 1;   % 1 en centro del DISCO
+A = double(bwdist(M));                          % para calcular matriz de distancia
+M(A<radio(1))=1;
+D=D&M;
+C=C&M;
+
+figure(1)
+subplot(121)
+imshowpair(B,D)
+title('DISK')
+subplot(122)
+imshowpair(B,C)
+title('CUP')
+pause(0.02)
+
+end
