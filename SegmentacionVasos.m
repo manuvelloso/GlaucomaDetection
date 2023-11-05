2b475f9700245de0c40a2c57eb4e3e4d0fac9ae3
Manuela Velloso <65735915+manuvelloso@users.noreply.github.com>
2023-11-05 16:02:23 -0300

    Update SegmentacionVasos.m

--- SegmentacionVasos.m
+++ SegmentacionVasos.m
@@ -1,6 +1,6 @@
 %% Segementacion de vasos
 % Inputs: imagen (im) 
-% Outputs: mascara binarizada con los vasos sangu韓eos del disco
+% Outputs: mascara binarizada con los vasos sangu铆neos del disco
 
 function Vasos = SegmentacionVasos(im)
 % Preprocesamiento de Imagen
@@ -10,27 +10,28 @@ Ic(Ic<=thr) = thr;
 Ic = Ic - min(Ic(:));
 Ic = Ic ./ max(Ic(:));
 
-V2 = vesselness2D(Ic, 0.5:0.5:2.5, [1;1], 0.3, false);
+V2 = vesselness2D(Ic, 0.5:0.5:2.5, [1;1], 0.1, false);
 
 aux = V2;                   %Variable auxiliar
 V2(:,:,2)=0;                %Canal Green a cero
 V2(:,:,3)=0;                %Canal Blue a cero
 
-Result = aux - V2;          %Eliminaci髇 de componente roja
-Result(:,:,3)=0;            %Solo componente verde (los vasos)
+G = aux - V2;          %Eliminaci贸n de componente roja
+G(:,:,3)=0;            %Solo componente verde (los vasos)
 
-% Binarizaci髇
-th = multithresh(Result,1);
-bw = im2bw(Result,th);
+% Binarizaci贸n
+th = multithresh(G,1);
+bw_G = im2bw(G,th);
 
-% Operaciones Morfol骻icas
-bw1 = bwmorph(bw,'clean');
-bw2 = bwareaopen(bw1,16);
-bw3 = bwmorph(bw2,'skel');
+% Operaciones morfol贸gicas
+bw_G = bwmorph(bw_G,'clean');
+bw_G = bwmorph(bw_G,'thin');
+bw_G = bwareaopen(bw_G,6);
 
-% Obtenci髇 de la m醩cara del disco 
+% Obtenci贸n de la m谩scara del disco 
 mask = MascaraDisco(im);
 
-% Vasos sangu韓eos del disco
-Vasos = mask.*bw3;
+% Vasos sangu铆neos del disco
+Vasos = mask.*bw_G;
+
 end
