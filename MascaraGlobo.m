807f6407e985b635bd0ea89282a241c2f295cb76
Victoria Guarnieri <vickyguarnieri1@gmail.com>
2023-11-18 18:30:44 -0300

    Delete MascaraGlobo.m

--- MascaraGlobo.m
+++
@@ -1,7 +0,0 @@
-function D = MascaraGlobo(size, centro, radio)
-M = zeros(size);                  % matriz de ceros
-M(centro(2),centro(1)) = 1;       % 1 en centro de aorta
-D = double(bwdist(M));            % calculo matriz con distancias
-D(D<=radio) = 1;                  % distancia es menor al radio, la pinto
-D(D>radio) = 0;
-end
